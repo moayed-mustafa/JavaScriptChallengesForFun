4a0ca5db56208621d106aeeedd419423dc11cd1e
Moayed Mustafa <moayedmustafa@Moayeds-MacBook-Pro.local>
2019-10-22 16:20:42 +0400

    updating the solution for problem7

--- problem7.js
+++ problem7.js
@@ -6,79 +6,48 @@
 //   20: 2,
 //   1: 4
 // }
-const billBreakDown = (cost)=>{
+const billBreakDown = (amount)=>{
+  // declare the variables 
+    let billsArray = [100, 50, 20, 10, 5,1]
+    const solutionObj = {100:0,
+  50:0,
+  20:0,
+  10:0,
+  5:0,
+  1:0};
+  let modulus; 
+  let numOfBills;
+    //  loop through the bills array
+  billsArray.forEach(bill =>{
+    // start calculation is there is money to breakdown
+    if(amount > 0){
+      console.log('Checking ' + bill)
+     modulus = amount % bill;
+     console.log(`modulus=  ${modulus}  bill = ${bill}` )
+     numOfBills = amount/ bill;
+     console.log(`numOfBills=  ${parseInt(numOfBills)}  `)
+    //  the amount is completely divisible by the first bill
+    if(modulus === 0){
+      solutionObj[bill]+=  numOfBills
+      amount = modulus
   
-
-  
-  // solving the problem in a hypothetical situation where the lowest bill is 5
-  // create an object to store the values in
-const solutionObj = {100:0,
-50:0,
-20:0,
-10:0,
-5:0};
-// figure out how many hundreds you can get out of the total amount
-let hundredsWithDecimals = cost / 100
-let hundredsWithOutDecimals =parseInt(cost / 100)
-let remaining = hundredsWithDecimals - hundredsWithOutDecimals
-remaining = remaining  * 100;
-remaining = Math.round(remaining)
-
-// update the solution object
-solutionObj[100]+= hundredsWithOutDecimals;
-  
-  
-
-  
-// figure out how many fifties in the remaining amount
-
-let fiftiesWithDecimals = remaining / 50;
-let fiftiesWithOutDecimals = parseInt(fiftiesWithDecimals);
-remaining = fiftiesWithDecimals - fiftiesWithOutDecimals;
-remaining = remaining * 10
-remainig =  Math.round(remaining)
-// update the solution object
-solutionObj[50]+= fiftiesWithOutDecimals;
-
-// since every cost number im inputting can be divided by 5, update the remaining amount by multiplying it by 5
-remaining = remaining * 5
-remaining= Math.round(remaining)
-// console.log(`remaining ${remaining}$`)
-
-// figure out how many twinties are in the remainig amount
-let twintiesWithDecimals = remaining /20;
-let twintiesWithOutDecimals = parseInt(twintiesWithDecimals)
-// update the remaining
-remaining = twintiesWithDecimals - twintiesWithOutDecimals;
-remaining = (remaining / 5)* 100
-remaining = Math.round(remaining)
-// update the solution object
-solutionObj[20]+= twintiesWithOutDecimals
-// console.log(`remaining ${remaining}$`)
-
-// figure out how many tens are in the remaining amount
-if(remaining >= 10){
-  let tensWithDecimals = remaining / 10
-  let tensWithOutDecimals = parseInt(tensWithDecimals)
-  remaining = tensWithDecimals - tensWithOutDecimals;
-  remaining*=10;
-  remaining = Math.round(remaining)
-  // console.log(`remaining ${remaining}$`)
-  // update the object solution
-  solutionObj[10]+= tensWithOutDecimals;
-  solutionObj[5]+= remaining;
-
-}else{
-  remaining = remaining / 5
-  solutionObj[5]+= remaining;
-  // return `remaining ${remaining}`;
-
-}
-
-console.log(solutionObj);
-
-
-return solutionObj
-}
-billBreakDown(5250)
+   }
+   else{
+     // update the amount
+     amount = modulus;
+    //  amount = amount - (parseInt(numOfBills) * bill)
+        
+    //   parse number of bills 
+    numOfBills = parseInt(numOfBills)
+    // updat the object
+    solutionObj[bill] += numOfBills
+    console.log(solutionObj[bill]+ '*' + '$' + bill)
+   }
+  }
+  console.log('/////////////////////')
+  })
+  console.log(solutionObj)
+  return  solutionObj
+  }
+  billBreakDown(15036)
 
